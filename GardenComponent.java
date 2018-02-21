c14eda6c103b14d7bf6c80da525c71a97349a758
DGanama <daoudganama@gmail.com>
2018-02-21 15:23:39 -0600

    Delete GardenComponent.java

--- GardenComponent.java
+++
@@ -1,9 +0,0 @@
-import javafx.geometry.Point2D;
-import javafx.scene.paint.Color;
-
-public interface GardenComponent {
-	public void move(double x, double y);
-	public boolean ContainsPoint(Point2D point);
-	public Point2D getCurrentCenter();
-	public void changeColor(Color newColor);
-}
